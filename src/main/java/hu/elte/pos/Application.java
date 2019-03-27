8c90db940e4e4fa60914b97a5deb2414a092cd0e
wkz6f3 <kittyikeh@gmail.com>
2019-03-27 14:29:05 +0100

    Add basic login window

--- src/main/java/hu/elte/pos/Application.java
+++ src/main/java/hu/elte/pos/Application.java
@@ -5,6 +5,7 @@ import hu.elte.pos.view.ApplicationView;
 public class Application {
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(ApplicationView::new);
+        //javax.swing.SwingUtilities.invokeLater(ApplicationView::new Login);
+        new Login();
     }
 }
