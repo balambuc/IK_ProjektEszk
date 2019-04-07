8882a7675ba1aa1293447c9bdde81187542fc6df
abnagy <43534410+abnagy@users.noreply.github.com>
2019-04-07 23:15:06 +0200

    add basic tests

--- src/test/java/hu/elte/pos/ApplicationTest.java
+++ src/test/java/hu/elte/pos/ApplicationTest.java
@@ -1,20 +1,35 @@
 package hu.elte.pos;
 
+import hu.elte.pos.controller.ItemController;
+import hu.elte.pos.controller.TableController;
+import hu.elte.pos.controller.WaiterController;
 import hu.elte.pos.view.ApplicationView;
 
 import org.fest.swing.fixture.FrameFixture;
 import org.junit.After;
 import org.junit.Before;
+//import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 
 
 class ApplicationTest {
 
+    TableController tc;
+    WaiterController wc;
+    ItemController ic;
+
     private FrameFixture applicationView;
 
-    @Before
+    @BeforeEach
     void setUp() {
-        applicationView = new FrameFixture(new ApplicationView());
+        tc = new TableController();
+        wc = new WaiterController();
+        ic = new ItemController();
     }
 
     @After
@@ -22,8 +37,42 @@ class ApplicationTest {
         applicationView.cleanUp();
     }
 
+
     @Test
-    void initApplication() {
-        assert(applicationView == null);
+    void testTables(){
+        tc.addTable();
+        tc.addTable();
+        assertTrue(tc.getTable(1).getTableNumber() == 2);
     }
+
+    @Test
+    void testWaiters(){
+        wc.addWaiter(26, "liza", "1234asd");
+        wc.addWaiter(19, "csabi", "4567asd");
+
+        assertTrue(wc.getWaiter(1).getName()== "csabi");
+    }
+
+    @Test
+    void testItems(){
+        ic = new ItemController();
+        ic.addItem(1000, "palacsinta");
+        ic.addItem(300, "kóla");
+        ic.addItem(3000, "bélszín");
+        boolean vanepalacsinta= false;
+        for(int i = 0; i<ic.getNumberOfItems(); i++){
+            if(ic.getItem(i).getName().equals("palacsinta")){
+                vanepalacsinta = true;
+            }
+        }
+
+        assertTrue(vanepalacsinta);
+    }
+
+
+
+
+
+
+
 }
