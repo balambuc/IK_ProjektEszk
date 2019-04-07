fd50dfed139089c1da053431a341a37314aafc6b
abnagy <43534410+abnagy@users.noreply.github.com>
2019-04-07 20:59:59 +0200

    add more classes and functions to model and controller

--- src/main/java/hu/elte/pos/controller/WaiterController.java
+++ src/main/java/hu/elte/pos/controller/WaiterController.java
@@ -1,4 +1,25 @@
 package hu.elte.pos.controller;
 
+import hu.elte.pos.model.WaiterModel;
+
+import java.util.ArrayList;
+
 public class WaiterController {
+
+    private ArrayList<WaiterModel> waiterList= new ArrayList<>();
+
+    public WaiterController(){}
+
+    public void addWaiter(int age, String name, String id){
+        waiterList.add(new WaiterModel(name, age, id));
+    }
+
+    public WaiterModel getWaiter(int index){
+        return waiterList.get(index);
+    }
+
+    public void removeWaiter(int index){
+        waiterList.remove(index);
+    }
+
 }
